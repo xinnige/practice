97be553a42edfa92b2a17879e22a3843cbf7f76d
xinni-ge <xinni.ge@ntt.com>
2016-03-23 10:30:35 +0900

    simplify trailing0

--- practice/python/trailing0.py
+++ practice/python/trailing0.py
@@ -1,23 +1,12 @@
 class Solution(object):
-    def get_majority(self, n, factor):
-        power = factor
-        shang = n/power
-        counter = shang
-        while shang > 0:
-            power = power*factor   
-            shang = n/power
-            counter += shang
-        return counter 
-        
     def trailingZeroes(self, n):
         """
         :type n: int
         :rtype: int
         """
-        num5 = self.get_majority(n, 5)
-        num2 = self.get_majority(n, 2)
-        print num5
-        return min(num5, num2)
+        if n < 5:
+            return 0
+        return self.trailingZeroes(n/5) + n/5 
 
 sol = Solution()
 print sol.trailingZeroes(1)
