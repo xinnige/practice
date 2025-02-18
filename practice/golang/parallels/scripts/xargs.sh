d7842fc0ea023d3d2ed83af60cca8b466594a744
xinnige <xinni.ge1990@gmail.com>
2025-02-18 12:13:18 +0800

    Add rust build

--- practice/golang/parallels/scripts/xargs.sh
+++ practice/golang/parallels/scripts/xargs.sh
@@ -1,3 +1,7 @@
+
 xargs -I {} -P 20 echo {} < <(printf '%s\n' {1..10})
 
 seq 10 | xargs -I X bash -c "echo X \$RANDOM"
+
+
+xargs -I {} -P 10 curl -u admin:123456 http://127.0.0.1/index.html  < <(printf '%s\n' {1..20})
